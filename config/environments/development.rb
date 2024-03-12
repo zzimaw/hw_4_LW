a5f43e8c40aac7246d4f3c061338e430f70bc48e
Linda Wang <linda.wang@kellogg.northwestern.edu>
2024-03-12 00:08:51 +0000

    image_upload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
   
   # So we don't get the "Cannot render console" message in Gitpod
   config.web_console.whiny_requests = false
+  config.active_storage.service = :local
 end
